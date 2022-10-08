a28e077e7b1b8aeb7cb39b7ff55a87778f1b4ddb
sagar <skatahre@gmail.com>
2022-10-09 00:42:56 +0530

    hello

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Programming_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("  Logical  Programming  Problems ");
+        }
+    }
+}
